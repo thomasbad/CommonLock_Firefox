40e4c10c7d6f8b8cf6f0b044cba15032e47cc938
mkaa <20796385+thomasbad@users.noreply.github.com>
2022-08-05 18:16:48 +0800

    Create enable.bat

---
+++ enable.bat
@@ -0,0 +1,26 @@
+ECHO OFF
+CLS
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla" /F
+::reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox\Extensions\Locked" /V 1 /T REG_SZ /F /D "ApplicationGuardRel@microsoft.com"
+::Remove the comment above if Application Guard have been use here
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V BlockAboutConfig /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V BlockAboutAddons /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V BlockAboutProfiles /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V BlockAboutSupport /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableDeveloperTools /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableFeedbackCommands /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableFirefoxAccounts /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableFirefoxStudies /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableProfileImport /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableProfileRefresh /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableSecurityBypass /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableSafeMode /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V DisableSafeMode /T REG_DWORD /F /D 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox" /V Preferences /T REG_MULTI_SZ /F /D "7b000000200020002200620072006f00770073\00650072002e00640069007300610062006c00650052006500\730065007400500072006f006d007000740022003a0020007b\00000020002000200020002200560061006c00750065002200\3a00200031002c000000200020002000200022005300740061\0074007500730022003a00200022006c006f0063006b006500\640022000000200020007d0000000000"
+ECHO ===================COMPLETED===================
+ECHO -----------------------------------------------
+ECHO Enabled locking Firefox
+ECHO -----------------------------------------------
+ECHO ===========PRESS ANY KEY TO CONTINUE===========
+pause > NUL
+cls
